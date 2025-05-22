3abe5895a4292da946d7ae2c30519990720902df
Karl Yu <yh975593284@gmail.com>
2025-05-22 18:25:55 +0800

    add basics and ecc operations

--- contracts/hello-world/src/lib.rs
+++ contracts/hello-world/src/lib.rs
@@ -1,180 +1,343 @@
+//Note this is just draft implementation of honkVerifier logic over stellar soroban
+//We haven't implemented BN254 precompile for now , so use arkwork's bn254 instead
 #![no_std]
-
 extern crate alloc;
 use wee_alloc::WeeAlloc;
 
+//We keep this allocator as WASM need a memory allocator to handle dynamic memory allocation.
 #[global_allocator]
 static ALLOC: WeeAlloc = WeeAlloc::INIT;
 
-use ark_bn254::{Bn254, Fr, G1Affine, G1Projective as G1, G2Affine, G2Projective as G2};
-use ark_ec::{pairing::Pairing, CurveGroup, Group};
-use ark_serialize::{
-    CanonicalDeserialize, CanonicalSerialize, Compress, SerializationError, Validate,
+use ark_bn254::{Bn254, Fq, Fr, G1Affine, G2Affine ,Fq12};
+use ark_ec::{pairing::Pairing, AffineRepr, CurveGroup};
+use ark_ff::{Field, PrimeField, Zero,One};
+use ark_serialize::{CanonicalDeserialize, CanonicalSerialize};
+use soroban_sdk::{
+    contract, contractimpl, crypto, panic_with_error, symbol_short, vec, Bytes,BytesN, Env, Vec, U256,
 };
-use core::ops::Mul;
-//use ark_ff::{Field, PrimeField};
-use soroban_sdk::{contract, contractimpl, Bytes, Env};
 
-#[contract]
-pub struct BN254Contract;
+const N: u32 = 131072;
+const LOG_N: u32 = 17;
+const NUMBER_OF_PUBLIC_INPUTS: u32 = 4;
+const CONST_PROOF_SIZE_LOG_N: usize = 28;
+const NUMBER_OF_SUBRELATIONS: usize = 26;
+const BATCHED_RELATION_PARTIAL_LENGTH: usize = 8;
+const NUMBER_OF_ENTITIES: usize = 40;
+const NUMBER_UNSHIFTED: usize = 35;
+const NUMBER_TO_BE_SHIFTED: usize = 5;
+const NUMBER_OF_ALPHAS: usize = 25;
 
-fn serialize_g1(point: &G1, env: &Env) -> Bytes {   
-    let mut temp_vec = alloc::vec::Vec::new();  
-    point  
-        .into_affine()  
-        .serialize_with_mode(&mut temp_vec, Compress::No)  
-        .unwrap();  
-      
-    Bytes::from_slice(env, &temp_vec)  
+// Error codes
+#[derive(Clone, Debug)]
+#[repr(u32)]
+pub enum Error {
+    ProofLengthWrong = 0,
+    PublicInputsLengthWrong = 1,
+    SumcheckFailed = 2,
+    ShpleminiFailed = 3,
+    InvalidProof = 4,
 }
 
-fn deserialize_g1(bytes: &[u8]) -> Result<G1, SerializationError> {
-    let point = G1Affine::deserialize_with_mode(bytes, Compress::No, Validate::Yes)?;
-    Ok(point.into())
+#[derive(Clone, Debug)]
+pub struct G1Point {
+    x: Fq, //from ark_bn254
+    y: Fq, //from ark_bn254
 }
 
-// fn serialize_g2(env: &Env, point: &G2) -> Bytes {
-//     let mut temp_vec = Vec::new();
-//     point.into_affine().serialize_with_mode(&mut temp_vec, Compress::No).unwrap();
-
-//     Bytes::from_slice(env, &temp_vec)
+/// Performs checks to ensure that the point is on the curve and is in the right subgroup.
+//  pub fn new(x: P::BaseField, y: P::BaseField) -> Self {
+//     let point = Self {
+//         x,
+//         y,
+//         infinity: false,
+//     };
+//     assert!(point.is_on_curve());
+//     assert!(point.is_in_correct_subgroup_assuming_on_curve());
+//     point
 // }
+impl G1Point {
+    fn to_affine(&self) -> Result<G1Affine, Error> {
+        let point = G1Affine::new_unchecked(self.x, self.y);
+        if !point.is_on_curve() {
+            return Err(Error::InvalidProof);
+        }
 
-fn deserialize_g2(bytes: &[u8]) -> Result<G2, SerializationError> {
-    let point = G2Affine::deserialize_with_mode(bytes, Compress::No, Validate::Yes)?;
-    Ok(point.into())
+        if !point.is_in_correct_subgroup_assuming_on_curve() {
+            return Err(Error::InvalidProof);
+        }
+        Ok(point)
+    }
 }
 
-fn serialize_fr(env: &Env, scalar: &Fr) -> Bytes {
-    let mut temp_vec = alloc::vec::Vec::new();
-    scalar.serialize_with_mode(&mut temp_vec, Compress::No).unwrap();
-
-    Bytes::from_slice(env, &temp_vec)
+#[derive(Clone, Debug, Copy)]
+pub struct G1ProofPoint {
+    x_0: Fq,
+    x_1: Fq,
+    y_0: Fq,
+    y_1: Fq,
 }
 
-fn deserialize_fr(bytes: &[u8]) -> Result<Fr, SerializationError> {
-    Fr::deserialize_with_mode(bytes, Compress::No, Validate::Yes)
+/*
+  Example usage :
+    let mut reader = BytesReader::new(&proof_bytes);
+    let proof_point = G1ProofPoint::from_reader(&mut reader);
+    let g1_point = proof_point.to_g1_point();
+*/
+impl G1ProofPoint {
+    //read proof bytes and turn it into struct G1ProofPoint
+    fn from_reader(reader: &mut BytesReader) -> Self {
+        Self {
+            x_0: reader.read_fq(),
+            x_1: reader.read_fq(),
+            y_0: reader.read_fq(),
+            y_1: reader.read_fq(),
+        }
+    }
+
+    //convert struct G1ProofPoint to actual G1Point
+    fn to_g1_point(&self) -> G1Point {
+        let shift = Fq::from(2u128).pow(&[136u64]);
+        let x = self.x_0 + self.x_1 * shift;
+        let y = self.y_0 + self.y_1 * shift;
+        G1Point { x, y }
+    }
 }
 
+#[derive(Clone, Debug)]
+pub struct VerificationKey {
+    circuit_size: u32,
+    log_circuit_size: u32,
+    public_inputs_size: u32,
+    q_m: G1Affine,
+    q_c: G1Affine,
+    q_l: G1Affine,
+    q_r: G1Affine,
+    q_o: G1Affine,
+    q_4: G1Affine,
+    q_lookup: G1Affine,
+    q_arith: G1Affine,
+    q_delta_range: G1Affine,
+    q_elliptic: G1Affine,
+    q_aux: G1Affine,
+    q_poseidon2_external: G1Affine,
+    q_poseidon2_internal: G1Affine,
+    s1: G1Affine,
+    s2: G1Affine,
+    s3: G1Affine,
+    s4: G1Affine,
+    id1: G1Affine,
+    id2: G1Affine,
+    id3: G1Affine,
+    id4: G1Affine,
+    t1: G1Affine,
+    t2: G1Affine,
+    t3: G1Affine,
+    t4: G1Affine,
+    lagrange_first: G1Affine,
+    lagrange_last: G1Affine,
+}
 
+//proof
+#[derive(Clone, Debug)]
+pub struct Proof {
+    circuit_size: u32,
+    public_inputs_size: u32,
+    public_inputs_offset: u32,
+    w1: G1ProofPoint,
+    w2: G1ProofPoint,
+    w3: G1ProofPoint,
+    w4: G1ProofPoint,
+    z_perm: G1ProofPoint,
+    lookup_read_counts: G1ProofPoint,
+    lookup_read_tags: G1ProofPoint,
+    lookup_inverses: G1ProofPoint,
+    sumcheck_univariates: [[Fr; BATCHED_RELATION_PARTIAL_LENGTH]; CONST_PROOF_SIZE_LOG_N],
+    sumcheck_evaluations: [Fr; NUMBER_OF_ENTITIES],
+    gemini_fold_comms: [G1ProofPoint; CONST_PROOF_SIZE_LOG_N - 1],
+    gemini_a_evaluations: [Fr; CONST_PROOF_SIZE_LOG_N],
+    shplonk_q: G1ProofPoint,
+    kzg_quotient: G1ProofPoint,
+}
 
-#[contractimpl]
-impl BN254Contract {
-    pub fn generate_g1_point(env: Env, scalar: Bytes) -> Bytes {
-        let mut buffer = [0u8; 32];
+struct BytesReader<'a> {
+    bytes: &'a [u8],
+    pos: usize, //position
+}
 
-        let scalar_fr = if scalar.len() as usize == buffer.len() {
-            scalar.copy_into_slice(&mut buffer);
-            match deserialize_fr(&buffer[..]) {
-                Ok(s) => s,
-                Err(_) => Fr::from(1u32),
+impl<'a> BytesReader<'a> {
+    fn new(bytes: &'a [u8]) -> Self {
+        Self { bytes, pos: 0 }
+    }
+    fn read_u32(&mut self) -> u32 {
+        let mut arr = [0u8; 4];
+        arr.copy_from_slice(&self.bytes[self.pos..self.pos + 4]);
+        self.pos += 4;
+        u32::from_be_bytes(arr)
+    }
+    fn read_fq(&mut self) -> Fq {
+        let mut arr = [0u8; 32];
+        arr.copy_from_slice(&self.bytes[self.pos..self.pos + 32]);
+        self.pos += 32;
+        Fq::from_be_bytes_mod_order(&arr)
+    }
+    fn read_fr(&mut self) -> Fr {
+        let mut arr = [0u8; 32];
+        arr.copy_from_slice(&self.bytes[self.pos..self.pos + 32]);
+        self.pos += 32;
+        Fr::from_be_bytes_mod_order(&arr)
+    }
+}
+
+impl Proof {
+    pub fn from_bytes(bytes: &[u8]) -> Result<Self, Error> {
+        let mut reader = BytesReader::new(bytes);
+
+        let circuit_size = reader.read_u32();
+        let public_inputs_size = reader.read_u32();
+        let public_inputs_offset = reader.read_u32();
+
+        let w1 = G1ProofPoint::from_reader(&mut reader);
+        let w2 = G1ProofPoint::from_reader(&mut reader);
+        let w3 = G1ProofPoint::from_reader(&mut reader);
+        let lookup_read_counts = G1ProofPoint::from_reader(&mut reader);
+        let lookup_read_tags = G1ProofPoint::from_reader(&mut reader);
+        let w4 = G1ProofPoint::from_reader(&mut reader);
+        let lookup_inverses = G1ProofPoint::from_reader(&mut reader);
+        let z_perm = G1ProofPoint::from_reader(&mut reader);
+
+        // sumcheck_univariates
+        let mut sumcheck_univariates =
+            [[Fr::zero(); BATCHED_RELATION_PARTIAL_LENGTH]; CONST_PROOF_SIZE_LOG_N];
+        for i in 0..CONST_PROOF_SIZE_LOG_N {
+            for j in 0..BATCHED_RELATION_PARTIAL_LENGTH {
+                sumcheck_univariates[i][j] = reader.read_fr();
             }
-        } else {
-            Fr::from(1u32)
-        };
-
-        let g1_gen = G1::generator();
-        let point = g1_gen.mul(scalar_fr);
-
-         serialize_g1(&point,&env)
-      
-    }
-
-    pub fn scalar_mul_g1(env: Env, point_bytes: Bytes, scalar_bytes: Bytes) -> Bytes {
-        let mut point_buffer = [0u8; 48];
-        if point_bytes.len() as usize != point_buffer.len() {
-            return Bytes::from_slice(&env, &[0u8]);
         }
-        point_bytes.copy_into_slice(&mut point_buffer);
 
-        let point = match deserialize_g1(&point_buffer[..]) {
-            Ok(p) => p,
-            Err(_) => return Bytes::from_slice(&env, &[0u8]),
-        };
-
-        let mut scalar_buffer = [0u8; 32];
-        if scalar_bytes.len() as usize != scalar_buffer.len() {
-            return Bytes::from_slice(&env, &[0u8]);
+        // sumcheck_evaluations
+        let mut sumcheck_evaluations = [Fr::zero(); NUMBER_OF_ENTITIES];
+        for i in 0..NUMBER_OF_ENTITIES {
+            sumcheck_evaluations[i] = reader.read_fr();
         }
-        scalar_bytes.copy_into_slice(&mut scalar_buffer);
 
-        let scalar = match deserialize_fr(&scalar_buffer[..]) {
-            Ok(s) => s,
-            Err(_) => return Bytes::from_slice(&env, &[0u8]),
-        };
-
-        let result = point.mul(scalar);
-
-       serialize_g1(&result,&env)
-   
-    }
-
-    pub fn add_g1_points(env: Env, point1_bytes: Bytes, point2_bytes: Bytes) -> Bytes {
-        let mut point1_buffer = [0u8; 48];
-        if point1_bytes.len() as usize != point1_buffer.len() {
-            return Bytes::from_slice(&env, &[0u8]);
+        // gemini_fold_comms
+        let mut gemini_fold_comms = [G1ProofPoint {
+            x_0: Fq::zero(),
+            x_1: Fq::zero(),
+            y_0: Fq::zero(),
+            y_1: Fq::zero(),
+        }; CONST_PROOF_SIZE_LOG_N - 1];
+        for i in 0..(CONST_PROOF_SIZE_LOG_N - 1) {
+            gemini_fold_comms[i] = G1ProofPoint::from_reader(&mut reader);
         }
-        point1_bytes.copy_into_slice(&mut point1_buffer);
 
-        let point1 = match deserialize_g1(&point1_buffer[..]) {
-            Ok(p) => p,
-            Err(_) => return Bytes::from_slice(&env, &[0u8]),
-        };
-
-        let mut point2_buffer = [0u8; 48];
-        if point2_bytes.len() as usize != point2_buffer.len() {
-            return Bytes::from_slice(&env, &[0u8]);
+        // gemini_a_evaluations
+        let mut gemini_a_evaluations = [Fr::zero(); CONST_PROOF_SIZE_LOG_N];
+        for i in 0..CONST_PROOF_SIZE_LOG_N {
+            gemini_a_evaluations[i] = reader.read_fr();
         }
-        point2_bytes.copy_into_slice(&mut point2_buffer);
 
-        let point2 = match deserialize_g1(&point2_buffer[..]) {
-            Ok(p) => p,
-            Err(_) => return Bytes::from_slice(&env, &[0u8]),
-        };
+        let shplonk_q = G1ProofPoint::from_reader(&mut reader);
+        let kzg_quotient = G1ProofPoint::from_reader(&mut reader);
 
-        let result = point1 + point2;
-
-        serialize_g1(&result,&env)
-       
-    }
-
-    pub fn perform_pairing(env: Env, g1_bytes: Bytes, g2_bytes: Bytes) -> Bytes {
-        // For G1 point deserialization
-        let mut g1_buffer = [0u8; 48]; // Adjust size based on G1 point serialization format
-        if g1_bytes.len() as usize != g1_buffer.len() {
-            return Bytes::from_slice(&env, &[0u8]);
-        }
-        g1_bytes.copy_into_slice(&mut g1_buffer);
-
-        let g1_point = match deserialize_g1(&g1_buffer[..]) {
-            Ok(p) => p,
-            Err(_) => return Bytes::from_slice(&env, &[0u8]),
-        };
-
-        // For G2 point deserialization
-        let mut g2_buffer = [0u8; 96]; // Adjust size based on G2 point serialization format
-        if g2_bytes.len() as usize != g2_buffer.len() {
-            return Bytes::from_slice(&env, &[0u8]);
-        }
-        g2_bytes.copy_into_slice(&mut g2_buffer);
-
-        let g2_point = match deserialize_g2(&g2_buffer[..]) {
-            Ok(p) => p,
-            Err(_) => return Bytes::from_slice(&env, &[0u8]),
-        };
-
-        // Fix the unused variable warning by prefixing with underscore
-        let _pairing_result = Bn254::pairing(g1_point, g2_point);
-
-        // Use the pairing result directly without conditional compilation
-        let mut temp_vec = alloc::vec::Vec::new();
-        _pairing_result
-            .0
-            .serialize_with_mode(&mut temp_vec, Compress::No)
-            .unwrap();
-        Bytes::from_slice(&env, &temp_vec)
+        Ok(Proof {
+            circuit_size,
+            public_inputs_size,
+            public_inputs_offset,
+            w1,
+            w2,
+            w3,
+            w4,
+            z_perm,
+            lookup_read_counts,
+            lookup_read_tags,
+            lookup_inverses,
+            sumcheck_univariates,
+            sumcheck_evaluations,
+            gemini_fold_comms,
+            gemini_a_evaluations,
+            shplonk_q,
+            kzg_quotient,
+        })
     }
 }
 
-#[cfg(test)]  
+pub struct Transcript {
+    env: Env,
+    buffer: Bytes,
+}
+
+
+impl Transcript {
+    pub fn new(env: &Env) -> Self {
+        Self {
+            env: env.clone(),
+            buffer: Bytes::new(env),
+        }
+    }
+
+    pub fn append_bytes(&mut self, bytes: &[u8]) {
+         // Create a Bytes object from the raw slice  
+        let bytes_obj = Bytes::from_slice(&self.env, bytes);  
+        // Use copy_from_slice or extend_from_slice directly on buffer  
+        self.buffer.extend_from_slice(&bytes);
+    }
+
+    pub fn append_u256(&mut self, value: &U256) {
+        let bytes: Bytes = value.to_be_bytes(); 
+        let mut arr = [0u8; 32];
+        bytes.copy_into_slice(&mut arr);
+        self.buffer.extend_from_slice(&arr);
+    }
+
+    pub fn get_challenge(&mut self) -> BytesN<32> {
+        let crypto = self.env.crypto();
+        let hash = crypto.keccak256(&self.buffer);
+        self.buffer = Bytes::new(&self.env);
+        hash.to_bytes()
+    }
+}
+
+
+//ec operaitons 
+pub fn combine_bytes<F: PrimeField>(a: &F, b: &F) -> F {
+    let shift = F::from(2u128).pow(&[136u64]);
+    *a + *b * shift
+}
+
+pub fn convert_proof_point<F: PrimeField>(input: &G1ProofPoint) -> G1Point {  
+    let x = combine_bytes(&input.x_0, &input.x_1);  
+    let y = combine_bytes(&input.y_0, &input.y_1);  
+    G1Point { x, y }  
+}  
+  
+pub fn ec_add<G: CurveGroup>(point0: G::Affine, point1: G::Affine) -> G::Affine {  
+    (point0 + point1).into_affine()  
+}  
+  
+pub fn ec_mul<G: CurveGroup>(point: G::Affine, scalar: G::ScalarField) -> G::Affine {  
+    (point * scalar).into_affine()  
+}  
+  
+pub fn ec_sub<G: CurveGroup>(point0: G::Affine, point1: G::Affine) -> G::Affine {
+    (point0.into_group() - point1.into_group()).into_affine()
+}
+
+pub fn pairing_check(
+    rhs: G1Affine,
+    lhs: G1Affine,
+    fixed_g2: G2Affine,
+    vk_g2: G2Affine,
+) -> bool {
+    let pairing1 = Bn254::pairing(&rhs, &fixed_g2);
+    let pairing2 = Bn254::pairing(&lhs, &vk_g2);
+
+    let prod = pairing1.0 * pairing2.0;
+
+    prod == Fq12::one()
+}
+
+
+
+#[cfg(test)]
 mod test;
